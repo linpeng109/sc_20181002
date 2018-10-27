5a18b6d753ffc6c4f35e52ec9ac721d4813a44f7
linpeng109 <linpeng109@aliyun.com>
2018-10-27 16:13:15 +0800

    20181027

--- quartzServer/src/main/java/com/cn/schedule/ScheduleTasks.java
+++ quartzServer/src/main/java/com/cn/schedule/ScheduleTasks.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,6 +16,12 @@ import java.util.Date;
 @Log4j2
 public class ScheduleTasks {
 
+    private RestTemplate restTemplate = new RestTemplate();
+
+    private String getObjectByRestTemplate() {
+        return restTemplate.getForObject("http://localhost:8080/v1/webapp/userbase/findAll/1/5", String.class);
+    }
+
     private String currentTime() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         return dateFormat.format(new Date()).toString();
@@ -22,11 +29,11 @@ public class ScheduleTasks {
 
     @Scheduled(fixedRate = 5 * 1000)
     public void reportCurrentByFixeRate() {
-        log.debug("Current Time (per 5 seconds):" + currentTime());
+        log.debug("Fixed Rate mode. Per five seconds. Current Time is " + currentTime());
     }
 
     @Scheduled(cron = "0 */1 *  * * * ")
     public void reportCurrentByCron() {
-        log.debug("Current Time(per 1 minutes):" + currentTime());
+        log.debug("Cron mode. Per minutes.Get Object By RestTemplate : " + this.getObjectByRestTemplate());
     }
 }
